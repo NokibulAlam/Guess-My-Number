6e6485df268c0b8243616c444d77121e39a4d564
NokibulAlam <nokibul.alam02@gmail.com>
2022-03-23 02:34:22 +0600

    highscore feature added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -16,6 +17,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+    if(score > highScore) highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
   }
 
   else if(guess > secretNumber) {
